7ed8d16c3133b89002c7b75b0f625885477b5777
PRANJAL <pranjals0105@gmail.com>
2024-05-05 00:50:31 +0530

    added

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const userRoute = require("../backend/routes/userRoute");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 mongoose
-  .connect(
-    "mongodb+srv://pranjalsrivastavwork:1234567890@cluster0.n7fwj8z.mongodb.net/?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGODB)
   .then(() => {
     console.log("connected to database");
   })
   .catch(() => {
     console.log("connection failed");
   });
+console.log(process.env.MONGODB);
 app.use(express.json());
 app.use("/users", userRoute);
+let port = process.env.PORT || 3000;
 app.listen(3000, () => {
   console.log("server started at port 3000");
 });
